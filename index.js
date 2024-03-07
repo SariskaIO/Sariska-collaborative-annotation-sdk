0099bdfc3c7239d2e409b23cb1665ebee978b385
gurudeep88 <gurudeep88@gmail.com>
2024-03-07 11:39:32 +0530

    annotated image saved 8

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import SariskaCollaborativeAnnotation from './src/index';
 
 import * as Utils from './src/utils/index';
 
-export default SariskaCollaborativeAnnotation;
-
 export {Utils};
+
+export default SariskaCollaborativeAnnotation;
